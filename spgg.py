8b7d0e9507055b19b334fd7070621f7c72391d53
dotscha <dotsch@gmail.com>
2017-08-10 13:52:18 +0200

    update, almost there

--- spgg.py
+++ spgg.py
@@ -1,30 +1,47 @@
 import sys
 
-param = ord('.')
-separator = ord(' ')
+param = ord('.')		# will be $ff
+separator = ord(' ')	# will be $ea
 
 pseudoByte = 'byt'
 
 dataFile = sys.argv[1]
 patternsFile = sys.argv[2]
+streamsFile = None if len(sys.argv)<4 else sys.argv[3]
+
+outPrefix = "output"
 
 data = list(bytearray(open(dataFile,'rb').read()))
 dataAddr = data[0]+256*data[1]
 data = data[2:]
 
-# read and parse patterns
+# read and parse patterns and streams
 patterns = list(bytearray(open(patternsFile,'rb').read()))[2:]
 patterns.append(separator)
 
-pattern = []
+streams = None
+if streamsFile!=None:
+	streams = list(bytearray(open(streamsFile,'rb').read()))[2:]
+	streams.append(separator)
+	if len(patterns)!=len(streams) or not all([(p==param or p==s) for (p,s) in zip(patterns,streams)]):
+		print ("Patterns and streams are not aligned.",file=sys.stderr)
+		exit(1)
+else:
+	streams = [(c if c!=param else 0) for c in patterns]
+	
+pattern = {}
 for i in range(patterns.count(separator)):
 	sep = patterns.index(separator)
 	patt = patterns[:sep]
+	stre = streams[:sep]
 	if len(patt):
-		pattern.append(patt)
+		pattern[tuple(patt)] = stre
 	patterns = patterns[sep+1:]
+	streams = streams[sep+1:]
 
+print ("data")
 print (data)
+print ("pattern")
 print (pattern)
 
 # check if matching
@@ -35,7 +52,7 @@ def matching(data, pos, patt):
 			return False
 	return True
 
-#match parameters
+# match parameters
 def matchParams(data, pos, patt):
 	posEnd = min(len(data),pos+len(patt))
 	mp = []
@@ -60,7 +77,7 @@ while pos<len(data):
 	if best!=None:
 		matches.append(best)
 		pos += len(best[0])
-		print (best)
+		print ("match: {m}".format(m=best))
 	else:
 		print ("ERROR: No pattern found at {a}".format(a=hex(dataAddr+pos)),file=sys.stderr)
 		exit(1)
@@ -68,72 +85,116 @@ while pos<len(data):
 if len(data)<pos:
 	print ("WARNING: Partial match at the end overrun by {i} bytes.".format(i=pos-len(data)))
 
-
-usedPatterns = []
-for m in matches:
-	if not m[0] in usedPatterns:
-		usedPatterns.append(m[0])
+usedPatterns = {tuple(m[0]) for m in matches}
 
 if len(usedPatterns)>255:
 	print ("ERROR: Too many patterns: {i}".format(i=len(usedPatterns)),file=sys.stderr)
 	exit(1)
 
 # printing the speedcode generator
-def patternWriter(patt):
+def patternWriter(patt,stre):
 	for i in range(len(patt)):
 		c = patt[i]
 		if c!=param:
-			print ("\tlda #{v}".format(v=c))
+			print ("\tlda #{v}".format(v=c),file=outGen)
 		else:
-			print ("\tjsr get_param")
-		print ("\tsta (dest),y")
+			print ("\tjsr get_param{s}".format(s="" if stre[i]==0 else "_{i}".format(i=stre[i])),file=outGen)
+		print ("\tsta (dest),y",file=outGen)
 		if i+1<len(patt):
-			print ("\tiny")
-	print ("\tjmp cont_next")
+			print ("\tiny",file=outGen)
+	print ("\tjmp cont_next",file=outGen)
 
-print ("speedcode_generator:")
-print ("\tldx #0")
-print ("writer_loop:")
-print ("\tjsr get_param")
-print ("\ttay")
-print ("\tlda patt_write_lo,y")
-print ("\tsta jump_to")
-print ("\tlda patt_write_hi,y")
-print ("\tsta jump_to+1")
-print ("\tldy #0")
-print ("jump_to = *+1")
-print ("\tjmp $ffff")
-print ("cont_next:")
-print ("\tsec")
-print ("\ttya")
-print ("\tadc dest")
-print ("\tsta dest")
-print ("\tbcc writer_loop")
-print ("\tinc dest+1")
-print ("\tjmp writer_loop")
+outGen = open(outPrefix+"_spgen.asm",'w')
 
-print ("")
-print ("get_param:")
-print ("\tlda speedcode_data,x")
-print ("\tinx")
-print ("\tbne patt_writer_0")
-print ("\tinc get_param+2")
+print ("speedcode_generator:",file=outGen)
+print ("\tldx #0",file=outGen)
+print ("writer_loop:",file=outGen)
+print ("\tjsr get_param",file=outGen)
+print ("\ttay",file=outGen)
+print ("\tlda patt_write_lo,y",file=outGen)
+print ("\tsta jump_to",file=outGen)
+print ("\tlda patt_write_hi,y",file=outGen)
+print ("\tsta jump_to+1",file=outGen)
+print ("\tldy #0",file=outGen)
+print ("jump_to = *+1",file=outGen)
+print ("\tjmp $ffff",file=outGen)
+print ("cont_next:",file=outGen)
+print ("\tsec",file=outGen)
+print ("\ttya",file=outGen)
+print ("\tadc dest",file=outGen)
+print ("\tsta dest",file=outGen)
+print ("\tbcc writer_loop",file=outGen)
+print ("\tinc dest+1",file=outGen)
+print ("\tjmp writer_loop",file=outGen)
 
-print ("")
-print ("patt_writer_0:")
-print ("\trts")
+print ("",file=outGen)
+print ("get_param:",file=outGen)
+print ("\tlda speedcode_data,x",file=outGen)
+print ("\tinx",file=outGen)
+print ("\tbne patt_writer_0",file=outGen)
+print ("\tinc get_param+2",file=outGen)
+
+print ("",file=outGen)
+print ("patt_writer_0:",file=outGen)
+print ("\trts",file=outGen)
 
 
 pattIdx = list(zip(usedPatterns,range(1,1+len(usedPatterns))))
 
 for patt,idx in pattIdx:
-	print ("patt_writer_{i}".format(i=idx))
-	patternWriter(patt)
+	print ("patt_writer_{i}".format(i=idx),file=outGen)
+	patternWriter(patt, pattern[patt])
 
-print ("")
-print ("patt_writer_lo:")
+print ("",file=outGen)
+print ("patt_writer_lo:",file=outGen)
 for patt,idx in pattIdx:
-	print ("\t{o} <patt_writer_{i}".format(o=pseudoByte,i=idx))
-print ("patt_writer_hi:")
+	print ("\t{o} <patt_writer_{i}".format(o=pseudoByte,i=idx),file=outGen)
+print ("patt_writer_hi:",file=outGen)
 for patt,idx in pattIdx:
-	print ("\t{o} >patt_writer_{i}".format(o=pseudoByte,i=idx))
+	print ("\t{o} >patt_writer_{i}".format(o=pseudoByte,i=idx),file=outGen)
+
+outGen.close()
+
+# saving data into streams
+
+def extractStreams(patt,stre):
+	return [s for p,s in zip(patt,stre) if p==param]
+	
+def splitDataIntoStreams(data, stre, m):
+	for d,s in zip(data,stre):
+		if s in m:
+			m[s].append(d)
+		else:
+			m[s] = [d]
+	return m
+
+def writeStream(s,data,comment=None):
+	print (s)
+	print (data)
+	if not s in outData:
+		outData[s] = open(outPrefix+"_spdata_{s}.asm".format(s=s),'w')
+		print ("speedcode_data_{s}:".format(s=s),file=outData[s])
+	print (
+		"\t{o} {l}{c}".format(
+			o = pseudoByte,
+			l = ",".join([hex(d).replace("0x","$") for d in data]),
+			c = "" if comment==None else "\t;"+comment
+		),
+		file = outData[s]
+	)
+	
+outData = { 0 : open(outPrefix+"_spdata.asm",'w')}
+print ("speedcode_data:",file=outData[0])
+
+pattIdx = { patt:(i,extractStreams(patt,pattern[patt])) for patt,i in pattIdx }
+
+print ("pattIdx")
+print (pattIdx)
+
+for patt,params in matches:
+	ds = splitDataIntoStreams(params,pattIdx[patt][1],{0:[pattIdx[patt][0]]})
+	print (ds)
+	for s,data in ds.items():
+		writeStream(s,data)
+writeStream(0,[0],"the end")
+	
